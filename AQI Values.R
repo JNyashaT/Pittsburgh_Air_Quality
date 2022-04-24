807fd8fc870655f4bbb762b63f5f019afeeb784f
Jahzara Taylor <85776056+JNyashaT@users.noreply.github.com>
2022-04-24 11:18:06 -0400

    Add files via upload

---
+++ AQI Values.R
@@ -0,0 +1,25 @@
+#Air quality in Allegheny county
+library(sf)
+library(ggplot2)
+library (readr)
+library(dplyr)
+data <- data.frame(read.csv(file.choose())) ## Open AQI file
+
+#arrange (data, desc(index_value))
+
+arrange (data, desc(site))
+
+grp <- c("site")
+
+data %>%
+  group_by(across(grp)) %>%
+  summarize(mean_index = mean(index_value)) ## Mean AQI values according to sites in the document
+
+mean_index = c(21.4, 8.54, 9.95, 17.1, 24.3, 38.1, 3.11, 18.7, 32.2, 35.1, 18.0, 22.2, 28.3, 20.5, 6.34)
+areas = c("Avalon", "Clairton", "Flag Plaza", "Glassport High Street", "Harrison Township", "Lawrenceville", "Lawrencevill 2", "Liberty", "Liberty2", "Lincoln", "North Braddock", "Parkway East", "Pittsburgh", "South Fayette", "West Mifflin") mean_index = c(21.4, 8.54, 9.95, 17.1, 24.3, 38.1, 3.11, 18.7, 32.2, 35.1, 18.0, 22.2, 28.3, 20.5, 6.34)
+
+average_aqi = data.frame(areas, mean_index)
+databar = average_aqi$mean_index
+names(databar) = average_aqi$areas
+databar
+
